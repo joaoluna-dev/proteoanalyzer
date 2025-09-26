f4f6464225972b78f2e362ef47a946b7ef00b31f
joaoluna-dev <joaogabrieldeluna@gmail.com>
2025-09-25 21:41:42 -0300

    Adição de função de plotagem e enriquecimento.

--- main.py
+++ main.py
@@ -26,6 +26,46 @@ def read_proteomics_file(rawfilepath, method, control):
         data = omics.OmicScope(rawfilepath, Method=method, ControlGroup=control)
     return data
 
+def plot_data(rawfiledata, analysis_type, titlename, plotsdir, analysis):
+    if analysis_type == "conditions_barplot" or analysis_type == "conditions_boxplot":
+        proteins_string = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
+        proteins_list = [p.strip() for p in proteins_string.split(',')]
+        palette = input("Insira a paleta desejada (pressione enter para utilizar a paleta de cores padrão): ").lower()
+        if len(palette) == 0:
+            palette = 'viridis'
+        print(f"Gerando {analysis}...")
+        rawfiledata.bar_protein(*proteins_list,
+                                  palette=palette,
+                                  dpi=300,
+                                  save=os.path.join(plotsdir, titlename))
+
+    elif analysis_type == "dynamic_range" or analysis_type == "ma_plot":
+            proteins_string = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
+            proteins_list = [p.strip() for p in proteins_string.split(',')]
+            print(f"Gerando {analysis}...")
+            rawfiledata.bar_protein(*proteins_list,
+                                    dpi=300,
+                                    save=os.path.join(plotsdir, titlename))
+
+    elif analysis_type == "correlation_heatmap" or analysis_type == "expression_heatmap":
+        print(f"Gerando {analysis}...")
+        rawfiledata.correlation(dpi=300,
+                                  linewidth=0,
+                                  save=os.path.join(plotsdir, titlename))
+
+    elif analysis_type == "id_barplot" or analysis_type == "pca" or analysis_type == "kmeans" or analysis_type == "normalization_plot":
+        print(f"Gerando {analysis}...")
+        rawfiledata.bar_protein(dpi=300,
+                                save=os.path.join(plotsdir, titlename))
+
+def perform_ora_enrichment(rawfiledata, db, titlename, analysis, tablesdir, plotsdir):
+    print(f"Iniciando análise: {analysis}...")
+    data = omics.EnrichmentScope(rawfiledata, Analysis='ORA', dbs=[db])
+    data_dataframe = pd.DataFrame(data)
+    create_xlsx_file(data_dataframe, titlename, tablesdir)
+    data.dotplot(dpi=300, palette='PuBu', save=os.path.join(plotsdir, f"{titlename}.tiff"))
+    print(f"Análise {analysis} concluída.")
+
 def create_xlsx_file(dataframe, name, tablesdir):
     filename = os.path.join(tablesdir, f"{name}.xlsx")
     dataframe.to_excel(filename)
@@ -35,7 +75,6 @@ def create_json_file(dictionary, name, tablesdir):
     with open(filename, "w+") as js:
         json.dump(dictionary, js)
 
-#----------------------------------------------------------------------------------#
 #----------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
@@ -58,177 +97,106 @@ if __name__ == "__main__":
             control_group = input("Insira o nome do grupo controle, exatamente como está na planilha: ")
 
             #Lendo os dados e criando os arquivos com as planilhas
+            #Obtendo os dados da proteômica a partir da planilha original
             print("Lendo o arquivo de dados...")
             raw_file_data = read_proteomics_file(raw_file_path, proteomics_method, control_group)
+
+            #Lendo os parÂmetros e salvando-os em um arquivo
             print("Criando tabela de parâmetros...")
             params_dataframe = pd.DataFrame(raw_file_data.Params())
             create_xlsx_file(params_dataframe, "Parâmetros", tables_dir)
+
+            #Criando arquivo json com as condições do estudo
             print("Criando arquivo de condições do estudo...")
             conditions_dictionary = {"Condições": f"{raw_file_data.Conditions}",
                                      "Controle": f"{raw_file_data.ControlGroup}"}
             create_json_file(conditions_dictionary, "Condições do estudo", tables_dir)
+
+            #Criando tabela com os dados brutos
             print("Criando tabela de dados brutos...")
             all_data_dataframe = pd.DataFrame(raw_file_data.quant_data())
             create_xlsx_file(all_data_dataframe, "Dados brutos", tables_dir)
+
+            #Criando tabela com os DEPs
             print("Criando tabela de DEPs...")
             deps_dataframe = pd.DataFrame(raw_file_data.deps())
             create_xlsx_file(deps_dataframe, "DEPs", tables_dir)
 
             #Plotando os gráficos
             print("Gerando gráficos...")
-            raw_file_data.bar_ident(logscale=True,
-                                    dpi=300,
-                                    save=os.path.join(plots_dir, f"{project_name}_Barplot_Identificação.tiff"))
 
-            dynamic_range_selection = input("Deseja plotar o gráfico de Dynamic range? (y/n): ").lower()
-            while True:
-                if dynamic_range_selection == "y":
-                    dynamic_range_proteins = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
-                    dynamic_range_proteins_list = [p.strip() for p in dynamic_range_proteins.split(',')]
-                    print("Gerando gráfico de Dynamic Range...")
-                    raw_file_data.DynamicRange(*dynamic_range_proteins_list,
-                                               dpi=300,
-                                               save=os.path.join(plots_dir, f"{project_name}_Dynamic_range.tiff"))
-                    break
-                elif dynamic_range_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {dynamic_range_selection}")
-                    continue
+            #Barplot de identificação
+            plot_data(raw_file_data, "id_barplot", f"{project_name}_Barplot_Identificação.tiff", plots_dir, "barplot de identificação")
+
+            #Dynamic range
+            plot_data(raw_file_data, "dynamic_range",  f"{project_name}_Dynamic_range.tiff", plots_dir, "gráfico de dynamic range")
 
             #Volcano_plot_selection = input("Deseja plotar o volcano plot? (y/n): ")
 
-            MA_plot_selection = input("Deseja plotar o grático MA plot? (y/n): ")
-            while True:
-                if MA_plot_selection == "y":
-                    MA_plot_proteins = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
-                    MA_plot_proteins_list = [p.strip() for p in MA_plot_proteins.split(',')]
-                    print("Gerando MA plot...")
-                    raw_file_data.MAplot(*MA_plot_proteins_list,
-                                         dpi=300,
-                                         save=os.path.join(plots_dir, f"{project_name}_MA_plot.tiff"))
-                    break
-                elif MA_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {MA_plot_selection}")
-                    continue
+            #MA plot
+            plot_data(raw_file_data, "ma_plot",  f"{project_name}_MA_plot.tiff", plots_dir, "gráfico de MA")
 
-            Normalization_plot_selection = input("Deseja plotar o gráfico de normalização? (y/n): ")
-            while True:
-                if Normalization_plot_selection == "y":
-                    print("Gerando gráfico de normalização...")
-                    raw_file_data.normalization_boxplot(dpi=300,
-                                                        save=os.path.join(plots_dir, f"{project_name}_Normalização_boxplot.tiff"))
-                    break
-                elif Normalization_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {Normalization_plot_selection}")
-                    continue
+            #Gráfico de normalização
+            plot_data(raw_file_data, "normalization_plot", f"{project_name}_Normalização_boxplot.tiff", plots_dir, "gráfico de normalização")
 
-            Proteins_bar_plot_selection = input("Deseja plotar o gráfico de abundância de proteinas entre as condições do estudo? (y/n): ")
-            while True:
-                if Proteins_bar_plot_selection == "y":
-                    Proteins_bar_plot_proteins = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
-                    Proteins_bar_plot_proteins_list = [p.strip() for p in Proteins_bar_plot_proteins.split(',')]
-                    print("Gerando gráfico de barras da abundância de proteínas...")
-                    raw_file_data.bar_protein(*Proteins_bar_plot_proteins_list,
-                                         palette='viridis',
-                                         dpi=300,
-                                         save=os.path.join(plots_dir, f"{project_name}_Bar_Plot_Proteínas_Entre_Condições.tiff"))
-                    break
-                elif Proteins_bar_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {Proteins_bar_plot_selection}")
-                    continue
+            #Barplot de proteínas entre as condições
+            plot_data(raw_file_data, "conditions_barplot", f"{project_name}_Bar_Plot_Proteínas_Entre_Condições.tiff", plots_dir, "gráfico barplot de comparação de proteínas entre condições")
 
-            Proteins_boxplot_selection = input("Deseja plotar o gráfico boxplot de abundância de proteinas entre as condições do estudo? (y/n): ")
-            while True:
-                if Proteins_boxplot_selection == "y":
-                    Proteins_boxplot_proteins = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula e os nomes entre aspas simples: ")
-                    Proteins_boxplot_proteins_list = [p.strip() for p in Proteins_boxplot_proteins.split(',')]
-                    print("Gerando boxplot da abundância de proteínas...")
-                    raw_file_data.boxplot_protein(*Proteins_boxplot_proteins_list,
-                                              palette='viridis',
-                                              dpi=300,
-                                              save=os.path.join(plots_dir, f"{project_name}_Boxplot_Proteínas_Entre_Condições.tiff"))
-                    break
-                elif Proteins_boxplot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {Proteins_boxplot_selection}")
-                    continue
+            #Boxplot de proteínas entre as condições
+            plot_data(raw_file_data, "conditions_boxplot", f"{project_name}_Boxplot_Proteínas_Entre_Condições.tiff", plots_dir, "gráfico boxplot de comparação de proteínas entre condições")
 
-            Heatmap_selection = input("Deseja plotar o heatmap do nível de expressão das proteínas? (y/n): ")
-            while True:
-                if Heatmap_selection == "y":
-                    print("Gerando heatmap de expressão...")
-                    raw_file_data.heatmap(dpi=300,
-                                          linewidth=0,
-                                          save=os.path.join(plots_dir, f"{project_name}_Heatmap_expressão.tiff"))
-                    break
-                elif Heatmap_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {Heatmap_selection}")
-                    continue
+            #Heatmap de expressão
+            plot_data(raw_file_data, "expression_heatmap", f"{project_name}_Heatmap_expressão.tiff", plots_dir, "gráfico heatmap de expressão")
 
-            Correlation_plot_selection = input("Deseja plotar o heatmap de correlação entre os grupos? (y/n): ")
-            while True:
-                if Correlation_plot_selection == "y":
-                    print("Gerando heatmap de correlação...")
-                    raw_file_data.correlation(dpi=300,
-                                          linewidth=0,
-                                          save=os.path.join(plots_dir, f"{project_name}_Heatmap_correlação.tiff"))
-                    break
-                elif Correlation_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {Correlation_plot_selection}")
-                    continue
+            #Heatmap de correlação
+            plot_data(raw_file_data, "correlation_heatmap", f"{project_name}_Heatmap_correlação.tiff", plots_dir, "gráfico heatmap de correlação")
 
-            PCA_plot_selection = input("Deseja plotar o gráfico de PCA? (y/n): ")
-            while True:
-                if PCA_plot_selection == "y":
-                    print("Gerando gráfico de PCA...")
-                    raw_file_data.pca(pvalue=0.05,
-                                      dpi=300,
-                                      save=os.path.join(plots_dir, f"{project_name}_PCA.tiff"))
-                    break
-                elif PCA_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {PCA_plot_selection}")
-                    continue
+            #PCA
+            plot_data(raw_file_data, "pca", f"{project_name}_PCA.tiff", plots_dir, "gráfico de PCA")
 
-            K_mean_plot_selection = input("Deseja plotar o gráfico de K-means? (y/n): ")
-            while True:
-                if K_mean_plot_selection == "y":
-                    print("Gerando gráfico de K-means...")
-                    raw_file_data.k_trend(dpi=300,
-                                          save=os.path.join(plots_dir, f"{project_name}_Kmeans.tiff"))
-                    break
-                elif K_mean_plot_selection == "n":
-                    break
-                else:
-                    print(f"Valor inválido inserido: {K_mean_plot_selection}")
-                    continue
-            print("Análise concluída com sucesso!")
+            #K-means
+            plot_data(raw_file_data, "kmeans", f"{project_name}_Kmeans.tiff", plots_dir, "gráfico de K-means")
+            print("Plotagem dos dados concluída.")
+
+            #Enriquecimento dos dados
+            print("Iniciando enriquecimento dos dados...")
+            #KEGG
+            perform_ora_enrichment(raw_file_data, 'KEGG_2021_Human', f"{project_name}_KEGG_pathways", "vias KEGG", tables_dir, plots_dir)
+
+            #GO: processo biológico
+            perform_ora_enrichment(raw_file_data, 'GO_Biological_Process_2025', f"{project_name}_GO_BP", "processo biológico", tables_dir, plots_dir)
+
+            #CC: componente celular
+            perform_ora_enrichment(raw_file_data, 'GO_Cellular_Component_2025', f"{project_name}_GO_CC", "componente celular", tables_dir, plots_dir)
+
+            #MF: função molecular
+            perform_ora_enrichment(raw_file_data, 'GO_Molecular_Function_2025', f"{project_name}_GO_MF", "função molecular", tables_dir, plots_dir)
+
+            #Reactome
+            perform_ora_enrichment(raw_file_data, 'Reactome_Pathways_2024', f"{project_name}_Reactome", "vias Reactome", tables_dir, plots_dir)
+
+            #OMIM
+            perform_ora_enrichment(raw_file_data, 'OMIM_Expanded', f"{project_name}_OMIM", "vias OMIM", tables_dir, plots_dir)
+
+            #DisGeNET
+            perform_ora_enrichment(raw_file_data, 'DisGeNET', f"{project_name}_DisGeNET", "vias DisGeNET", tables_dir, plots_dir)
+            print("Enriquecimento dos dados concluído.")
+            print("Análise dos dados concluída com sucesso!")
+
+            repeat = input("Deseja realizar uma nova análise? (y/n): ").lower()
+            if repeat == "y":
+                continue
+            elif repeat == "n":
+                break
+            else:
+                print(f"Valor invalido inserido: {repeat}.")
 
         except ValueError as error:
             print(f"O valor inserido é invalido: {error}")
         except Exception as error:
             print(f"Erro inesperado: {error}")
 
-        repeat = input("Deseja realizar uma nova análise? (y/n): ").lower()
-        if repeat == "y":
-            continue
-        elif repeat == "n":
-            break
-        else:
-            print(f"Valor invalido inserido: {repeat}.")
+
 
 
 
