1de5afda3c551f0428a9b405a21544d660aa5069
joaoluna-dev <joaogabrieldeluna@gmail.com>
2025-09-26 22:38:50 -0300

    Adição do sistema de plotagem de volcano plot, com possibilidade de definição de cutoff inferior, e ajustes nas mensagens imprimidas no console para melhor rastreamento do funcionamento

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 import json
 import omicscope as omics
 import pandas as pd
+import seaborn as sns
+import numpy as np
 import os
 from datetime import datetime
 import math
 import logging
 import shutil
+
+from matplotlib import pyplot as plt
 
 
 # ---------------------------------------------------------------------------------#
@@ -96,12 +100,12 @@ def plot_data(rawfiledata, analysis_type, titlename, plotsdir, analysis):
             rawfiledata.bar_protein(*proteins_list,
                                     palette=palette,
                                     dpi=300,
-                                    save=os.path.join(plotsdir, titlename))
+                                    save=os.path.join(plotsdir, f"{titlename}_"))
         else:  # conditions_boxplot
             rawfiledata.boxplot_protein(*proteins_list,
                                  palette=palette,
                                  dpi=300,
-                                 save=os.path.join(plotsdir, titlename))
+                                 save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "dynamic_range":
         proteins_string = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula. Certifique-se de que as proteínas indicadas estão na tabela, e que estão escritas exatamente como estão na tabela: ")
@@ -110,7 +114,7 @@ def plot_data(rawfiledata, analysis_type, titlename, plotsdir, analysis):
         logging.info(f"Gerando {analysis}...")
         rawfiledata.DynamicRange(*proteins_list,
                                   dpi=300,
-                                  save=os.path.join(plotsdir, titlename))
+                                  save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "ma_plot":
         proteins_string = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula. Certifique-se de que as proteínas indicadas estão na tabela, e que estão escritas exatamente como estão na tabela: ")
@@ -119,56 +123,112 @@ def plot_data(rawfiledata, analysis_type, titlename, plotsdir, analysis):
         logging.info(f"Gerando {analysis}...")
         rawfiledata.MAplot(*proteins_list,
                             dpi=300,
-                            save=os.path.join(plotsdir, titlename))
+                            save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "correlation_heatmap":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.correlation(dpi=300,
                                 linewidth=0,
-                                save=os.path.join(plotsdir, titlename))
+                                save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "expression_heatmap":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.heatmap(dpi=300,
                             linewidth=0,
-                            save=os.path.join(plotsdir, titlename))
+                            save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "id_barplot":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.bar_ident(dpi=300,
-                           save=os.path.join(plotsdir, titlename))
+                           save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "pca":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.pca(dpi=300,
-                        save=os.path.join(plotsdir, titlename))
+                        save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "kmeans":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.k_trend(dpi=300,
-                           save=os.path.join(plotsdir, titlename))
+                           save=os.path.join(plotsdir, f"{titlename}_"))
 
     elif analysis_type == "normalization_plot":
         print(f"Gerando {analysis}...")
         logging.info(f"Gerando {analysis}...")
         rawfiledata.normalization_boxplot(dpi=300,
-                                       save=os.path.join(plotsdir, titlename))
+                                       save=os.path.join(plotsdir, f"{titlename}_"))
+
+def create_volcano_plot(df, cutoff, pvalue_cutoff, plotsdir, titlename):
+    proteins_string = input("Insira as proteínas que deseja apontar no gráfico, separadas por vírgula. Certifique-se de que as proteínas indicadas estão na tabela, e que estão escritas exatamente como estão na tabela: ")
+    proteins_list = [p.strip() for p in proteins_string.split(',') if p.strip()]
+    full_path = os.path.join(plotsdir, f"{titlename}.svg")
+    df['regulation'] = 'Not Significant'
+    condition_up = (df['log2(fc)'] >= cutoff) & (df['pAdjusted'] < pvalue_cutoff)
+    condition_down = (df['log2(fc)'] <= -cutoff) & (df['pAdjusted'] < pvalue_cutoff)
+    df.loc[condition_up, 'regulation'] = 'Up-regulated'
+    df.loc[condition_down, 'regulation'] = 'Down-regulated'
+
+    # --- Plot Styling ---
+    plt.style.use('seaborn-v0_8-white')
+    plt.figure(figsize=(8, 8))
+
+    palette = {
+        'Up-regulated': '#ff4d4d',
+        'Down-regulated': '#4d4dff',
+        'Not Significant': 'darkgrey'
+    }
+
+    sns.scatterplot(
+        data=df, x='log2(fc)', y='-log10(pAdjusted)', hue='regulation',
+        palette=palette, s=40, alpha=0.7, edgecolor='none'
+    )
+
+    # --- Axis and threshold lines ---
+    plt.axvline(x=0, color='black', linestyle='-', linewidth=0.75)
+    plt.axhline(y=-np.log10(pvalue_cutoff), color='dimgrey', linestyle='--', linewidth=1)
+    plt.axvline(x=cutoff, color='dimgrey', linestyle='--', linewidth=1)
+    plt.axvline(x=-cutoff, color='dimgrey', linestyle='--', linewidth=1)
+
+    # --- Protein Labeling ---
+    df_labels = df[df['gene_name'].isin(proteins_list)]
+    for index, row in df_labels.iterrows():
+        plt.text(row['log2(fc)'], row['-log10(pAdjusted)'], row['gene_name'],
+                 ha='center', va='bottom', fontsize=9, fontweight='bold', alpha=0.9)
+
+    # --- Final Plot Customization ---
+    plt.title('Volcano Plot', fontsize=16, fontweight='bold')
+    plt.ylabel(r'$-log_{10}(Adjusted\ p-value)$', fontsize=12)
+    plt.xlabel(r'$log_2(Fold\ Change)$', fontsize=12)
+    plt.xlim(-3, 3)
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = ['Up-regulated', 'Down-regulated', 'Not Significant']
+    plt.legend([handles[labels.index(key)] for key in order], order, title='Regulation', frameon=False)
+
+    try:
+        print("Gerando volcano plot...")
+        plt.savefig(full_path, format='svg', dpi=300, bbox_inches='tight')
+        print(f"\nVolcano plot salvo com sucesso em: '{full_path}'")
+        logging.info(f"Volcano plot salvo com sucesso em: '{full_path}'")
+    except Exception as e:
+        print(f"Um erro ocorreu durante a geração do volcano plot: {e}")
+        logging.error(f"Um erro ocorreu durante a geração do volcano plot: {e}")
 
 
 def perform_ora_enrichment(rawfiledata, db, titlename, analysis, tablesdir, plotsdir):
-    print(f"Iniciando análise: {analysis}...")
+    print(f"Iniciando análise de: {analysis}...")
     logging.info(f"Iniciando análise: {analysis}...")
     data_object = omics.EnrichmentScope(rawfiledata, Analysis='ORA', dbs=[db])
     data_dataframe = data_object.results
     create_xlsx_file(data_dataframe, titlename, tablesdir)
-    data_object.dotplot(dpi=300, palette='PuBu', save=os.path.join(plotsdir, f"{titlename}.tiff"))
-    print(f"Análise {analysis} concluída.")
-    logging.info(f"Análise {analysis} concluída.")
+    data_object.dotplot(dpi=300, palette='PuBu', save=os.path.join(plotsdir, f"{titlename}_"))
+    print(f"Análise de {analysis} concluída.")
+    logging.info(f"Análise de {analysis} concluída.")
 
 
 def create_xlsx_file(dataframe, name, tablesdir):
@@ -289,43 +349,55 @@ if __name__ == "__main__":
             logging.info("Gerando gráficos...")
 
             # Barplot de identificação
-            plot_data(raw_file_data, "id_barplot", f"{project_name}_Barplot_Identificação.tiff", plots_dir,
+            print("Iniciando plotagem de barplot de identificação das condições...")
+            plot_data(raw_file_data, "id_barplot", project_name, plots_dir,
                       "barplot de identificação")
 
             # Dynamic range
-            plot_data(raw_file_data, "dynamic_range", f"{project_name}_Dynamic_range.tiff", plots_dir,
+            print("Iniciando plotagem de Dynamic Range...")
+            plot_data(raw_file_data, "dynamic_range", project_name, plots_dir,
                       "gráfico de dynamic range")
 
-            # Volcano_plot_selection = input("Deseja plotar o volcano plot? (y/n): ")
+            # Volcano plot
+            print("Iniciando plotagem de Volcano Plot...")
+            create_volcano_plot(deps_dataframe, log2fc_cutoff, 0.05, plots_dir, f"{project_name}_Volcano_plot")
 
             # MA plot
-            plot_data(raw_file_data, "ma_plot", f"{project_name}_MA_plot.tiff", plots_dir, "gráfico de MA")
+            print("Iniciando plotagem de MA plot...")
+            plot_data(raw_file_data, "ma_plot", project_name, plots_dir, "gráfico de MA")
 
             # Gráfico de normalização
-            plot_data(raw_file_data, "normalization_plot", f"{project_name}_Normalização_boxplot.tiff", plots_dir,
+            print("Iniciando plotagem de gráfico de normalização...")
+            plot_data(raw_file_data, "normalization_plot", project_name, plots_dir,
                       "gráfico de normalização")
 
             # Barplot de proteínas entre as condições
-            plot_data(raw_file_data, "conditions_barplot", f"{project_name}_Bar_Plot_Proteínas_Entre_Condições.tiff",
+            print("Iniciando plotagem de barplot comparando proteínas entre condições...")
+            plot_data(raw_file_data, "conditions_barplot", project_name,
                       plots_dir, "gráfico barplot de comparação de proteínas entre condições")
 
             # Boxplot de proteínas entre as condições
-            plot_data(raw_file_data, "conditions_boxplot", f"{project_name}_Boxplot_Proteínas_Entre_Condições.tiff",
+            print("Iniciando plotagem de boxplot comparando proteínas entre condições...")
+            plot_data(raw_file_data, "conditions_boxplot", project_name,
                       plots_dir, "gráfico boxplot de comparação de proteínas entre condições")
 
             # Heatmap de expressão
-            plot_data(raw_file_data, "expression_heatmap", f"{project_name}_Heatmap_expressão.tiff", plots_dir,
+            print("Iniciando plotagem de heatmap de expressão...")
+            plot_data(raw_file_data, "expression_heatmap", project_name, plots_dir,
                       "gráfico heatmap de expressão")
 
             # Heatmap de correlação
-            plot_data(raw_file_data, "correlation_heatmap", f"{project_name}_Heatmap_correlação.tiff", plots_dir,
+            print("Iniciando plotagem de heatmap de correlação...")
+            plot_data(raw_file_data, "correlation_heatmap", project_name, plots_dir,
                       "gráfico heatmap de correlação")
 
             # PCA
-            plot_data(raw_file_data, "pca", f"{project_name}_PCA.tiff", plots_dir, "gráfico de PCA")
+            print("Iniciando plotagem de gráfico de PCA...")
+            plot_data(raw_file_data, "pca", project_name, plots_dir, "gráfico de PCA")
 
             # K-means
-            plot_data(raw_file_data, "kmeans", f"{project_name}_Kmeans.tiff", plots_dir, "gráfico de K-means")
+            print("Iniciando plotagem de gráfico de PCA...")
+            plot_data(raw_file_data, "kmeans", project_name, plots_dir, "gráfico de K-means")
 
             print("Plotagem dos dados concluída.")
             logging.info("Plotagem dos dados concluída.")
@@ -335,26 +407,26 @@ if __name__ == "__main__":
             logging.info("Iniciando enriquecimento dos dados...")
 
             # KEGG
-            perform_ora_enrichment(raw_file_data, 'KEGG_2021_Human', f"{project_name}_KEGG_pathways", "vias KEGG",
+            perform_ora_enrichment(raw_file_data, 'KEGG_2021_Human', project_name, "vias KEGG",
                                    tables_dir, plots_dir)
 
             # GO: processo biológico
-            perform_ora_enrichment(raw_file_data, 'GO_Biological_Process_2025', f"{project_name}_GO_BP","processo biológico", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'GO_Biological_Process_2025', project_name,"processo biológico", tables_dir, plots_dir)
 
             # CC: componente celular
-            perform_ora_enrichment(raw_file_data, 'GO_Cellular_Component_2025', f"{project_name}_GO_CC", "componente celular", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'GO_Cellular_Component_2025', project_name, "componente celular", tables_dir, plots_dir)
 
             # MF: função molecular
-            perform_ora_enrichment(raw_file_data, 'GO_Molecular_Function_2025', f"{project_name}_GO_MF", "função molecular", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'GO_Molecular_Function_2025', project_name, "função molecular", tables_dir, plots_dir)
 
             # Reactome
-            perform_ora_enrichment(raw_file_data, 'Reactome_Pathways_2024', f"{project_name}_Reactome", "vias Reactome", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'Reactome_Pathways_2024', project_name, "vias Reactome", tables_dir, plots_dir)
 
             # OMIM
-            perform_ora_enrichment(raw_file_data, 'OMIM_Expanded', f"{project_name}_OMIM", "vias OMIM", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'OMIM_Expanded', project_name, "vias OMIM", tables_dir, plots_dir)
 
             # DisGeNET
-            perform_ora_enrichment(raw_file_data, 'DisGeNET', f"{project_name}_DisGeNET", "vias DisGeNET", tables_dir, plots_dir)
+            perform_ora_enrichment(raw_file_data, 'DisGeNET', project_name, "vias DisGeNET", tables_dir, plots_dir)
             print("Enriquecimento dos dados concluído.")
             logging.info("Enriquecimento dos dados concluído.")
             print("Análise dos dados concluída com sucesso!")
